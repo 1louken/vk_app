5fd4251b4bc863cc8e822b34411438f3f7ac5088
1louken <lesha.belikov2005@yandex.ru>
2021-08-20 22:32:40 +0300

    Update Persik.js

--- src/panels/Persik.js
+++ src/panels/Persik.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Panel, PanelHeader, Header, Group, PanelHeaderBack } from '@vkontakte/vkui';
+import { Panel, PanelHeader, Header, Button, Div, Group, PanelHeaderBack } from '@vkontakte/vkui';
 
 const startParams = new URLSearchParams(window.location.search)
 const userId = startParams.get("vk_user_id")
 
+function send() {
+	bridge
+	  .send('VKWebAppGetEmail')
+	  .then(data => {
+	    // Обработка события в случае успеха
+	    console.log(data.email);
+	  })
+	  .catch(error => {
+	    // Обработка события в случае ошибки
+	  });
+	};
+
 const Persik = props => (
 	<Panel id={props.id}>
 		<PanelHeader
-			left={<PanelHeaderBack onClick={props.go} data-to="home"/>}
-		>
-			Ваш id
-		</PanelHeader>
-
-
-		<Group header={<Header mode="secondary">Ваш id: {userId} </Header>}></Group>
+			left={<PanelHeaderBack onClick={props.go} data-to="home"/>}>Ваш id</PanelHeader>
+		<Group header={<Header mode="secondary">Ваш id: {userId} </Header>}>
+			<Div>
+				<Button stretched size="l" mode="secondary" onClick={send}>Уведомление</Button>
+			</Div>s
+		</Group>
 	</Panel>
 );
 
