eb1825013fa0bd54260b9d46c17377266738c6c8
1louken <lesha.belikov2005@yandex.ru>
2021-08-21 00:27:21 +0300

    Update Home.js

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -11,18 +11,18 @@ const Home = ({ id, go, fetchedUser }) => (
 
 		<Group>
 			<Div style={{display: 'flex'}}>
-       			<Button size="l" stretched style={{ marginRight: 8, marginLeft: 10}} stretched mode="secondary" before={<Icon24MenuOutline/>} onClick={go} data-to="menu">
-       				<Div style={{ marginRight: 8, marginLeft: 10, fontSize: 25 }}>
+       			<Button size="l" stretched style={{ marginRight: 8, marginLeft: 5}} stretched mode="secondary" before={<Icon24MenuOutline/>} onClick={go} data-to="menu">
+       				<Div style={{ fontSize: 25 }}>
        					Меню
        				</Div>
        			</Button>
        			<Button size="l" stretched style={{ marginRight: 8 }} stretched mode="secondary" before={<Icon28GraphOutline/>} onClick={go} data-to="top">
-       				<Div style={{  marginRight: 8, fontSize: 25 }}>
+       				<Div style={{ fontSize: 25 }}>
        					Рейтинг
        				</Div>
        			</Button>
-       			<Button size="l" stretched style={{ marginRight: 10 }} stretched mode="secondary" before={<Icon24StorefrontOutline/>} onClick={go} data-to="store">
-       				<Div style={{  marginRight: 10, fontSize: 25 }}>
+       			<Button size="l" stretched style={{ marginRight: 5 }} stretched mode="secondary" before={<Icon24StorefrontOutline/>} onClick={go} data-to="store">
+       				<Div style={{ fontSize: 25 }}>
        					Магазин
        				</Div>
        			</Button>
