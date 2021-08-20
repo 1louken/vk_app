69afb6145ccabc0fc53c7022a2442ace2b87134f
1louken <lesha.belikov2005@yandex.ru>
2021-08-21 00:03:38 +0300

    Update Home.js

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -13,7 +13,11 @@ const Home = ({ id, go, fetchedUser }) => (
 			<Div style={{display: 'flex'}}>
        			<Button size="l" stretched style={{ marginRight: 8 }} stretched mode="secondary" before={<Icon24MenuOutline/>} onClick={go} data-to="menu">Меню</Button>
        			<Button size="l" stretched style={{ marginRight: 8 }} stretched mode="secondary" before={<Icon28GraphOutline/>} onClick={go} data-to="top">Рейтинг</Button>
-       			<Button size="l" stretched mode="secondary" before={ <Icon24StorefrontOutline/>} onClick={go} data-to="store">Магазин</Button>
+       			<Button size="l" stretched mode="secondary" before={ <Icon24StorefrontOutline/>} onClick={go} data-to="store">
+       				<Div>
+       					Магазин
+       				</Div>
+       			</Button>
      		</Div>
 		</Group>
 	</Panel>
