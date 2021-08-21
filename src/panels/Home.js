529c05b6757bb87835c07bd0bd030a1c43c7d23b
1louken <lesha.belikov2005@yandex.ru>
2021-08-21 21:18:41 +0300

    Update Home.js

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -11,15 +11,22 @@ const Home = ({ id, go, fetchedUser }) => (
 
 		<Group>
 			<Div style={{display: 'flex'}}>
-       			<Button size="l" align="left" stretched style={{ marginRight: 8}} stretched mode="secondary" before={<Icon24MenuOutline/>} onClick={go} data-to="menu">
+       			<Button size="l" stretched style={{ marginRight: 8}} stretched mode="secondary" before={<Icon24MenuOutline/>} onClick={go} data-to="menu">
        				Меню
        			</Button>
-       			<Button size="l" align="center" stretched style={{ marginRight: 8}} stretched mode="secondary" before={<Icon28GraphOutline/>} onClick={go} data-to="top">
+       			<Button size="l" stretched style={{ marginRight: 8}} stretched mode="secondary" before={<Icon28GraphOutline/>} onClick={go} data-to="top">
        				Рейтинг
        			</Button>
-       			<Button size="l" align="right" stretched style={{}} stretched mode="secondary" before={<Icon24StorefrontOutline/>} onClick={go} data-to="store">
+       			<Button size="l" stretched style={{}} stretched mode="secondary" before={<Icon24StorefrontOutline/>} onClick={go} data-to="store">
        				Магазин
        			</Button>
+
+
+       		</Div>
+       		<Div>
+       			<CellButton before={<Avatar shadow={false} size={40}><Icon24MenuOutline /></Avatar>}>Меню</CellButton>
+      			<CellButton before={<Avatar shadow={false} size={40}><Icon28GraphOutline /></Avatar>}>Рейтинг</CellButton>
+      			<CellButton before={<Avatar shadow={false} size={40}><Icon24StorefrontOutline /></Avatar>}>Магазин</CellButton>
      		</Div>
 		</Group>
 	</Panel>
